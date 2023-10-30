7ac7ed911a2a2fbc044f387e8ededc654e74777b
shazaaly <shaza.aly@gmail.com>
2023-10-31 00:22:08 +0200

    delete and link methods

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -9,6 +9,7 @@ from flask import abort, request, jsonify
 
 db_mode = os.getenv("HBNB_TYPE_STORAGE")
 
+
 @app_views.route("/places/<place_id>/amenities", strict_slashes=False,
                  methods=["GET"])
 def place_amenities(place_id):
@@ -27,3 +28,48 @@ def place_amenities(place_id):
     return jsonify(amenities_list)
 
 
+@app_views.route("/places/<place_id>/amenities/<amenity_id>",
+                 strict_slashes=False,
+                 methods=["DELETE"])
+def delete_amenity(place_id, amenity_id):
+    """deleye an amenity my id"""
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+    if db_mode == "db":
+        place_amenities = place.amenities
+    else:
+        place_amenities = place.amenities_id
+
+    for amenity in place_amenities:
+        if amenity.id == amenity_id:
+            amenity.delete()
+            amenity.save()
+        else:
+            abort(404)
+    return jsonify({}, 200)
+
+
+@app_views.route("places/<place_id>/amenities/<amenity_id>",
+                 strict_slashes=False,
+                 methods=["POST"])
+def link_amenity(place_id, amenity_id):
+    """Link Amenity to a Place"""
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+    if db_mode == "db":
+        place_amenities = place.amenities
+    else:
+        place_amenities = place.amenities_id
+
+    if amenity not in place_amenities:
+        place_amenities.append(amenity)
+    else:
+        return jsonify(amenity, 200)
